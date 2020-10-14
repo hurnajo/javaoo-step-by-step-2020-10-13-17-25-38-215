73d090779d81b0482787b1c46f2193f3e5fd7c25
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-14 22:09:00 +0800

    [OOP][Jose Leo Hurna] Feature - Object-Oriented Programming Exercise 10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,27 @@
 package practice10;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        if(klass.getLeader() != null){
+            if(klass.getLeader().getName().equals(getName())){
+                return super.introduce() + " I am a Student. I am Leader of " + klass.getDisplayName()+".";
+            }
+        }
+        return super.introduce() + " I am a Student. I am at " + klass.getDisplayName()+".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,60 @@
 package practice10;
 
-public class Teacher {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private Klass klass;
+    private List<Klass> classes;
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (classes == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+
+        if (getClasses().size() != 0) {
+            String classes = getClasses().stream().map(Klass::getNumber).map(String::valueOf).collect(Collectors.joining(", "));
+            return super.introduce() + " I am a Teacher. I teach Class " + classes + ".";
+        }
+
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        boolean isClassIncluded = getClasses().stream()
+                .map(Klass::getNumber)
+                .anyMatch(classNum -> classNum.equals(student.getKlass().getNumber()));
+
+        if (isClassIncluded) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
+
+    public boolean isTeaching(Student student) {
+        boolean isIncluded = getClasses().stream()
+                .map(Klass::getNumber)
+                .anyMatch(classNum -> classNum.equals(student.getKlass().getNumber()));
+        if (isIncluded) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
 }

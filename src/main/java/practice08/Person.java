e965c06ed5b188533c37b05a2df4af7ff97d9cb7
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-14 20:57:10 +0800

    [OOP][Jose Leo Hurna] Feature - Object-Oriented Programming Exercise 8

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,46 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                id == person.id &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, id);
+    }
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,28 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass.getLeader() == null) {
+            return super.introduce() + " " + "I am a Student. I am at Class " + klass.getNumber() + ".";
+        }
+        if (getName().equals(klass.getLeader().getName())) {
+            return super.introduce() + " " + "I am a Student. I am Leader of Class " + klass.getNumber() + ".";
+        }
+        return super.introduce() + " " + "I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
 }

a89e917916ef76e0dd2405c2e6109cc2a94891b9
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-14 21:34:59 +0800

    [OOP][Jose Leo Hurna] Feature - Object-Oriented Programming Exercise 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,35 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        if (leader.getKlass().number != number) {
+            System.out.print("It is not one of us.\n");
+
+        } else {
+            this.leader = leader;
+        }
+    }
+
+    public void appendMember(Student student) {
+    }
 }
